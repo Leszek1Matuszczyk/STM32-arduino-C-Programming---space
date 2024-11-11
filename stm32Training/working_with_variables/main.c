332de1eb29d09fed956d7ec44450e1589c6fe04d
Leszek1Matuszczyk <149710969+Leszek1Matuszczyk@users.noreply.github.com>
2024-11-11 13:52:34 +0100

    Creatin some small training programs nothing special

---
+++ stm32Training/working_with_variables/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+char automatic[]="String variable with propr lenght";
+char suffix[50]= "Referetnt";
+char warning[5]= "bad idea";
+
+int main(int argc, char **argv)
+{
+	printf("default project\n");
+	return 0;
+}
