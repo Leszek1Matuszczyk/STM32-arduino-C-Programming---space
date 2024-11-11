57cc4cbb80420f8d51871a359dba4ea46906feb1
Leszek1Matuszczyk <149710969+Leszek1Matuszczyk@users.noreply.github.com>
2024-11-11 21:14:39 +0100

    adding few interesting xamples of typcasting in C

--- stm32Training/casting_types/main.c
+++ stm32Training/casting_types/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char **argv)
 {
@@ -8,7 +9,9 @@ int main(int argc, char **argv)
     int x = 12;
     
     float average = (x + y) / 2;
+    int sum = 0;
     
+    char word[30];
     
     printf("1/3 = %f\n", one_third);
     printf("average of %d and %d = %f \n",x ,y ,average);
@@ -20,4 +23,14 @@ int main(int argc, char **argv)
     
     printf("1/3 = %f\n", one_third);
     printf("average of %d and %d = %f \n",x ,y ,average);
+    printf("Sometimes you dont know how many argument function will have\n");
+    printf("Give me random word:\n");
+    scanf("%s",word);
+    
+    for (int i =0; i<strlen(word);i++)
+    {
+        sum = word[i]+sum; 
+        }
+    average = sum/(float)strlen(word);
+    printf("Average value of char in %s = %f",word,average);
 }
