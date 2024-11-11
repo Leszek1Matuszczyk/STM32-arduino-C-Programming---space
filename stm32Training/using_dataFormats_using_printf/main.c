ce358b2b84c5610558dfc7e1ae3cc5d92d45eb62
Leszek1Matuszczyk <149710969+Leszek1Matuszczyk@users.noreply.github.com>
2024-11-11 15:06:04 +0100

    some correction

--- stm32Training/using_dataFormats_using_printf/main.c
+++ stm32Training/using_dataFormats_using_printf/main.c
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
 	float one_half =0.5;
     double two_thirds = 0.66666666;
     double pi =  3.1415926535897932384626433; 
-    printf("1/2: %f\n", one_half);
-    printf("2/3: %f\n", two_thirds);
-    printf("Pi: %f\n", pi);
+    printf("1/2: |%5.2f|\n", one_half);
+    printf("2/3: |%12f|\n", two_thirds);
+    printf("Pi:  |%12.10f|\n", pi);
 }
