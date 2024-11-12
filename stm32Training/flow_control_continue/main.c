fd9454aa9b82412595ab77ec50ef9cea04ff5c33
Leszek1Matuszczyk <149710969+Leszek1Matuszczyk@users.noreply.github.com>
2024-11-12 14:17:49 +0100

    some more fun with flowcotrol

--- stm32Training/flow_control_continue/main.c
+++ stm32Training/flow_control_continue/main.c
@@ -7,36 +7,60 @@ if (1==1)
     
     printf(" 1==1 \n");
     
-    }
+    }else{
+        
+        printf("Condition 1 == 1 is false\n");
+        
+        }
 if (1!=1)
 {
     
     printf("1 != 1\n");
     
-    }
+    }else{
+        
+        printf("Condition 1 != 1 is false\n");
+        
+        }
 if (5<10)
 {
     
     printf("5 < 10\n");
     
-    }
+    }else{
+        
+        printf("Condition 5 < 10 is false\n");
+        
+        }
 if (5>10)
 {
     
     printf("5>10\n");
     
-    }
+    }else{
+        
+        printf("Condition 5 >10 is false\n");
+        
+        }
 if (12<=10)
 {
     
     printf("12<=10\n");
     
-    }
+    }else{
+        
+        printf("Condition 12<=10 is false\n");
+        
+        }
 if (12>=10)
 {
     
     printf("12>=10\n");
     
-    }
+    }else{
+        
+        printf("Condition 12>=10 is false\n");
+        
+        }
 	return 0;
 }
