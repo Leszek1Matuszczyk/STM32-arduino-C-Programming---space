c433ba27fbb96d49a6b2f15b67b244aa8ca4cba1
Leszek1Matuszczyk <149710969+Leszek1Matuszczyk@users.noreply.github.com>
2024-11-12 21:20:04 +0100

    Continuation of fun with loops

---
+++ stm32Training/build-Debug/for_loop_continuation/main.c.o.d
@@ -0,0 +1,1 @@
+../build-Debug/for_loop_continuation/main.c.o: main.c
---
+++ stm32Training/for_loop_continuation/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("Display only even numbers from 2 - 10\n");
+    for(int i =2; i <= 10; i= i+2)
+    {
+        printf("%d\n",i);
+        
+        }
+    printf("Count from 5 to 1\n");
+    for(int i =5 ; i>=1 ;i--)
+    {
+        printf("%d\n",i);
+    }
+}
