96acba5979e6f85e2ab2e5d5b2bd74a305dd0797
Leszek1Matuszczyk <149710969+Leszek1Matuszczyk@users.noreply.github.com>
2024-11-13 11:15:46 +0100

    training neesting on loop do_while program

--- stm32Training/do_While_Loop/main.c
+++ stm32Training/do_While_Loop/main.c
@@ -8,10 +8,16 @@ int main(int argc, char **argv)
     do{
         printf("Give me a number drom range 1-100 . 0 break loop\n");
         scanf("%d",&grade);
+        if(grade<=100 && grade>=1)
+        {
         total +=grade;
         count++;
+        }else if(grade!=0){
+            
+            printf("Wrong number if you want Quit choose 0 \n");
+            
+            }
         }while(grade!=0);
-    count--;
     if(count>0)
     {
         
