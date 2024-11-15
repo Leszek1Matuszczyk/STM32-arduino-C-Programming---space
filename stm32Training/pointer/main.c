d92b301d6d54fa5a4d0cb905f66eecc01ecf18e7
Leszek1Matuszczyk <149710969+Leszek1Matuszczyk@users.noreply.github.com>
2024-11-15 15:14:41 +0100

    some pointers and reminader what is it

---
+++ stm32Training/pointer/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int answer = 42;
+    double pi = 3.141592653589793238462643383279502884197;
+    printf("Value of answer variable : %d\n",answer);
+    printf("Pointer to variable answer :%p\n",answer);
+    printf("Value of pi variable %0.20f\n",pi);
+    printf("Pointer to pi variable %p\n",pi);
+}
