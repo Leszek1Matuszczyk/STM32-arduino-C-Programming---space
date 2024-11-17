332e6fe21b9726eec70155dc4f6e444478ec73b2
Leszek1Matuszczyk <149710969+Leszek1Matuszczyk@users.noreply.github.com>
2024-11-17 13:21:43 +0100

    some structures

---
+++ stm32Training/struct definition/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct transaction
+{
+
+    double amount;
+    int day,month,year;
+
+};
+
+void account(struct transaction trans)
+{
+
+    printf("Dollar  : %5.5df\n",trans.amount);
+    printf("Day     : %d\n",trans.day);
+    printf("Month   : %d\n",trans.month);
+    printf("Year    : %d\n",trans.year);
+
+}
+struct transaction Billing[50];
+int main()
+{
+    double saldo =0;
+    for(int i =0; i<50;i++)
+    {
+        printf("%d transaction\n",i+1);
+        printf("give Amount : ");
+        scanf("%lf",&Billing[i].amount);
+        printf("give Day : ");
+        scanf("%d",&Billing[i].day);
+        printf("give Month : ");
+        scanf("%d",&Billing[i].month);
+        printf("give Year : ");
+        scanf("%d",&Billing[i].year);
+        printf("Saldo : %lf\n",saldo += Billing[i].amount);
+    }
+
+}
