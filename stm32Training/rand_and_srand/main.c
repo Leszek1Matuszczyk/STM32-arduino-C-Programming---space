7ebe4d5e22804d116ec502a71cdfee5c878c92b8
Leszek1Matuszczyk <149710969+Leszek1Matuszczyk@users.noreply.github.com>
2024-11-17 18:21:25 +0100

    fun with rand()and srand function

---
+++ stm32Training/rand_and_srand/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    printf("RAND_MAX: %d \n",RAND_MAX);
+    unsigned int r1= rand();
+    printf("First random number: %d\n",r1);
+    srand(5);
+    printf("Second random number: %d \n",rand());
+    srand(time(NULL));
+    printf("Third random number: %d\n",rand());
+    unsigned int pin =rand() % 9000+1000;
+    printf("4 cipher random number: %d", pin);
+}
